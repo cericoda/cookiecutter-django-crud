c3b252e2cba3a103102850ec35e120b32fcbf57b
Kerry Channing <cericoda@altfa.com>
2014-04-12 22:04:59 +0100

    un_bug

--- {{cookiecutter.app_name}}/models.py
+++ {{cookiecutter.app_name}}/models.py
@@ -5,9 +5,9 @@ from django.utils.encoding import python_2_unicode_compatible
 from model_utils.models import StatusModel, TimeStampedModel
 from model_utils.choices import Choices
 
-class NonArchived{{ cookiecutter.model_name|capitalize }}Manager(models.Manager):
+class NonArchived{{ cookiecutter.model_name }}Manager(models.Manager):
     def get_query_set(self):
-        return super(NonArchived{{ cookiecutter.model_name|capitalize }}Manager, self).get_query_set().exclude(status='Archived')
+        return super(NonArchived{{ cookiecutter.model_name }}Manager, self).get_query_set().exclude(status='Archived')
 
 @python_2_unicode_compatible
 class {{ cookiecutter.model_name }}(StatusModel, TimeStampedModel):
@@ -17,7 +17,7 @@ class {{ cookiecutter.model_name }}(StatusModel, TimeStampedModel):
     name = models.CharField(max_length=255)
 
     all{{ cookiecutter.model_name|lower}} = models.Manager() # Establish this as the default/automatic manager
-    objects = NonArchived{{ cookiecutter.model_name|capitalize }}Manager()
+    objects = NonArchived{{ cookiecutter.model_name }}Manager()
 
     def __str__(self):
         return '{{ cookiecutter.model_name }} ({})'.format(self.id or 'Unsaved')
