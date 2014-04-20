914974fc72abf7440ef86755d5f5f4cdc9270882
Kerry Channing <cericoda@altfa.com>
2014-04-20 13:38:36 +0100

    some reversion + import-export goodness

--- {{cookiecutter.app_name}}/admin.py
+++ {{cookiecutter.app_name}}/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+from import_export.admin import ImportExportMixin, ImportExportModelAdmin
+from import_export import resources
+import reversion
 from .models import {{ cookiecutter.model_name }}
 
-admin.site.register({{ cookiecutter.model_name }})
+class IEAdmin(reversion.VersionAdmin, ImportExportModelAdmin):    
+    pass
+
+admin.site.register({{ cookiecutter.model_name }}, IEAdmin)
+
 
